b88053670976fdaa96db5cfb010c8e607f523468
Zach Sabin <zacharysabin@gmail.com>
2015-11-24 14:01:35 -0500

    Constructor
    Hopefully implemented construction? Have not yet tested

--- kdtree.py
+++ kdtree.py
@@ -1,3 +1,5 @@
+import numpy
+
 class BoundingBox:
     def __init__(self, triangle):
         self.upper, self.lower = self.createBoundingBox(triangle)
@@ -34,11 +36,45 @@ class Point:
         self.y = y
         self.z = z
 
-def pointToTriangle(t, p):
+#Finds the median for a list of boxes x,y,or z coordinate
+#At the moment it finds the median of the upper coordinates
+def findMedian(boxes, coord):   #coord must be either 0,1, or 2. 0 = x, 1 = y, 2 = z
+    coords = []
+    for box in boxes:
+        coords.append(box.upper[coord])
+    return numpy.median(coords)
+
+#Splits on the upper coordinate at the moment
+def splitBox(boxes, median, coord):
+    l1 = []
+    l2 = []
+    for box in boxes:
+        if box[coord] < median:
+            l1.append(box)
+        else:
+            l2.append(box)
+    return l1, l2
+
+
+def makeBox(boxes): #Takes a list of bounding boxes and gives a tight bounding box for them
+    maxes = []
+    mins = []
+    for coord in boxes[0].upper:    #Initialize the maxes and mins
+        maxes.append(coord)
+        mins.append(coord)
+    for box in boxes:
+        for i in range(3):
+            if (maxes[i] < box.upper[i]):
+                maxes[i] = box.upper[i]
+            if (mins[i] > box.lower[i]):
+                mins[i] = box.lower[i]
+    return BoundingBox(mins, maxes)
+
+def pointToTriangle(t, p):  #This actually just gives distance from point to point atm
     d = (p.x - t.x)**2 + (p.y - t.y)**2 + (p.z-t.z)**2
     return d
 
-def findClosestTriangle(point, rootBox):
+def findClosestTriangle(point, rootBox):        #Note this only finds closest box at the moment
     closestTriangle = None
     dist = None
     stack= []
@@ -46,7 +82,7 @@ def findClosestTriangle(point, rootBox):
     while not len(stack) == 0:
         print("iterating")
         box = stack.pop()
-        if (not closestTriangle is None) and ((point.x + dist < box.lower.x) or (point.x - dist > box.upper.x) or (point.y + dist < box.lower.y) or (point.y - dist > box.upper.y) or (point.z + dist < box.lower.z) or (point.z - dist > box.upper.z)):
+        if (not closestTriangle is None) and ((point.x + dist < box.lower[0]) or (point.x - dist > box.upper[0]) or (point.y + dist < box.lower[1]) or (point.y - dist > box.upper[1]) or (point.z + dist < box.lower[2]) or (point.z - dist > box.upper[2])):
                continue
         print ("past intersect check")
         if (box.leaf):
@@ -58,6 +94,35 @@ def findClosestTriangle(point, rootBox):
             for child in box.subBoxes:
                 stack.append(child)
     return closestTriangle
+
+def constructTree(boxes):
+    boxQueue = Queue()
+    listQueue = Queue()
+    b = makeBox(boxes)
+    b.coordSplit = 0
+    boxQueue.put(b)
+    listQueue.put(boxes)
+    while not boxQueue.empty():
+        curBox = boxQueue.get()
+        if curBox.leaf: #Potentially might need to pop from listqueue
+            continue
+        else:
+            curList = listQueue.get()
+            m = findMedian(curList, curBox.coordSplit)
+            l1,l2 = splitBox(curList, m, curBox.coordSplit)
+            box1 = makeBox(l1)
+            box2 = makeBox(l2)
+            c = (curBox.coordSplit + 1) % 3
+            box1.coordSplit = c
+            box2.coordSplit = c
+            curBox.subBoxes.append(box1)
+            curBox.subBoxes.append(box2)
+            listQueue.put(l1)
+            listQueue.put(l2)
+            boxQueue.put(box1)
+            boxQueue.put(box2)
+            
+        
 
 
 box = BoundingBox(Point(0,0,0), Point(5,5,0))
